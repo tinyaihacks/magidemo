e0cf6dfcb76cdbd42916bb638f10a45bd6d86830
alex buz <alex_buz@yahoo.com>
2024-03-23 23:48:48 -0400

    new

--- 2ll.py
+++ 2ll.py
@@ -3,8 +3,8 @@ from huggingface_hub import hf_hub_download
 from llama_cpp import Llama
 
 ## Download the GGUF model
-model_name = "TheBloke/Mixtral-8x7B-Instruct-v0.1-GGUF"
-model_file = "mixtral-8x7b-instruct-v0.1.Q4_K_M.gguf" # this is the specific model file we'll use in this example. It's a 4-bit quant, but other levels of quantization are available in the model repo if preferred
+model_name = "milkowski/Magicoder-S-DS-6.7B-GGUF"
+model_file = "magicoder-s-ds-6.7b.f16.gguf" # this is the specific model file we'll use in this example. It's a 4-bit quant, but other levels of quantization are available in the model repo if preferred
 model_path = hf_hub_download(model_name, filename=model_file)
 
 ## Instantiate model from downloaded file
@@ -24,7 +24,7 @@ generation_kwargs = {
 }
 
 ## Run inference
-prompt = "The meaning of life is "
+prompt = "can u code in wxpython "
 res = llm(prompt, **generation_kwargs) # Res is a dictionary
 
 ## Unpack and the generated text from the LLM response dictionary and print it
